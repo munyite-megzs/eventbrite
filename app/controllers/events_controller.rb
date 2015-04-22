b1561e379693dbd01a2c57e8a92280668014fc8e
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-04-22 19:52:48 +0300

    Sets the event_owner method as a before_filter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :event_owner!, only: [:edit, :update, :destroy]
   before_filter :authenticate_user!
 
   def index
