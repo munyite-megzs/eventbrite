8d4015efb7cf37f569e90e9610dc4df9169835fb
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-04-24 01:11:53 +0300

    Adds taggings events association

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,9 @@ class Event < ActiveRecord::Base
 
   belongs_to :organizer, class_name: "User"
 
+  has_many :taggings
+  has_many :tags, through: :taggings
+
   def slug_candidates
     [
       :name,
