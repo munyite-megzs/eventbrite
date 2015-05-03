e3920877353f043855bc59cae783752180df813d
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-05-03 23:46:27 +0300

    Adds attendance to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423220831) do
+ActiveRecord::Schema.define(version: 20150503204151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "attendances", ["event_id"], name: "index_attendances_on_event_id", using: :btree
+  add_index "attendances", ["user_id"], name: "index_attendances_on_user_id", using: :btree
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -77,6 +87,8 @@ ActiveRecord::Schema.define(version: 20150423220831) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "attendances", "events"
+  add_foreign_key "attendances", "users"
   add_foreign_key "taggings", "events"
   add_foreign_key "taggings", "tags"
 end
